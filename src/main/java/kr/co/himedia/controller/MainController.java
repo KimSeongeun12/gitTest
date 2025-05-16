4fe6d8efb3060b8a0295e17bab7c82b0e730072d
KimSeongeun <elex10147356@gmail.com>
2025-05-16 15:41:17 +0900

    브런치 초기화 설명

--- src/main/java/kr/co/himedia/controller/MainController.java
+++ src/main/java/kr/co/himedia/controller/MainController.java
@@ -14,6 +14,7 @@ public class MainController {
 	public String main() {
 		logger.info("Hello KimSeongeun");
 		logger.info("아무거나 추가!!!");
+		logger.info("브런치 초기화: 특정한 커밋의 상태로 완전히 되돌려버리는 것");
 		return "{\"msg\":\"Ok\"}";
 	}
 	
